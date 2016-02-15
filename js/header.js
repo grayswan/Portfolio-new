c4fed8d4766271cf05ff2bb1a1dc46499ee1ec02
Kristen Swan-Grashel <swan.kristen@gmail.com>
2016-02-15 17:22:04 -0500

    ch-ch-ch-changes

--- js/header.js
+++ js/header.js
@@ -9,25 +9,25 @@
     initAnimation();
 
     function initHeader() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        size = width > height ? height : width;
-        target = {x: 0, y: height};
+      width = window.innerWidth;
+      height = window.innerHeight;
+      size = width > height ? height : width;
+      target = {x: 0, y: height};
 
-        largeHeader = document.getElementById('large-header');
-        largeHeader.style.height = height+'px';
+      largeHeader = document.getElementById('large-header');
+      largeHeader.style.height = height+'px';
 
-        canvas = document.getElementById('canvas');
-        canvas.width = width;
-        canvas.height = height;
-        ctx = canvas.getContext('2d');
+      canvas = document.getElementById('canvas');
+      canvas.width = width;
+      canvas.height = height;
+      ctx = canvas.getContext('2d');
 
-        // create particles
-        lines = [];
-        for(var i = 0; i < 90; i++) {
-            var l = new Line(Math.random()*360);
-            lines.push(l);
-        }
+      // create particles
+      lines = [];
+      for(var i = 0; i < 90; i++) {
+          var l = new Line(Math.random()*360);
+          lines.push(l);
+      }
     }
 
     function initAnimation() {
@@ -76,8 +76,8 @@
 
         this.draw = function() {
 
-            var r1 = Math.random()*(size < 600 ? 600 : size)*0.6;
-            var r2 = Math.random()*(size < 600 ? 600 : size)*0.6;
+            var r1 = Math.random()*(size < 900 ? 900 : size)*0.9;
+            var r2 = Math.random()*(size < 900 ? 900 : size)*0.9;
             var x1 = r1*Math.cos(_this.angle*(Math.PI/180)) + width*.5;
             var y1 = r1*Math.sin(_this.angle*(Math.PI/180)) + height*0.48;
             var x2 = r2*Math.cos(_this.angle*(Math.PI/180)) + width*.5;
@@ -85,13 +85,13 @@
             ctx.beginPath();
             ctx.moveTo(x1, y1);
             ctx.lineTo(x2, y2);
-            ctx.strokeStyle = 'rgba(142,142,142,'+(0.5+Math.random()*0.5)+')';
+            ctx.strokeStyle = 'rgba(71,71,71,'+(0.5+Math.random()*0.5)+')';
 
             ctx.stroke();
 
             ctx.beginPath();
             ctx.arc(x1, y1, 2, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(135,135,135,'+(0.5+Math.random()*0.5)+')';
+            ctx.fillStyle = 'rgba(64,64,64,'+(0.5+Math.random()*0.5)+')';
             ctx.fill();
 
             _this.angle += Math.random();
